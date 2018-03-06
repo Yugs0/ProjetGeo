df9a934638ee34d9b34edfd5311b78229389d2e7
Hugo <hugo.vdh@hotmail.com>
2018-03-06 17:54:56 +0100

    Ça plante pas

--- app/src/main/java/com/example/hugo/projetgeo/MapsActivity.java
+++ app/src/main/java/com/example/hugo/projetgeo/MapsActivity.java
@@ -47,6 +47,7 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
 
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
